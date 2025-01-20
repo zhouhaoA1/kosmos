2ae08b144be763cf3a73386250ff56037497b25c
zhouhao_yewu <zhouhao_yewu@cmss.chinamobile.com>
2025-01-20 17:52:47 +0800

    fix: only check core pods ready in vc create/update progress

--- cmd/kubenest/operator/app/operator.go
+++ cmd/kubenest/operator/app/operator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -118,6 +119,7 @@ func SetupConfig(opts *options.Options) (*config.Config, error) {
 	c.Client = client
 	c.LeaderElection = opts.LeaderElection
 	c.KubeNestOptions = koc
+	c.CoreNamespaces = strings.Fields(strings.TrimSpace(opts.CoreNamespaces))
 
 	return c, nil
 }
@@ -269,6 +271,7 @@ func run(ctx context.Context, config *config.Config) error {
 		RootClientSet:   hostKubeClient,
 		KosmosClient:    kosmosClient,
 		KubeNestOptions: &config.KubeNestOptions,
+		CoreNamespaces:  config.CoreNamespaces,
 	}
 	if err = VirtualClusterInitController.SetupWithManager(mgr); err != nil {
 		return fmt.Errorf("error starting %s: %v", constants.InitControllerName, err)
